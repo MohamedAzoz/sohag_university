9dcf6389cf58444688b628a6e13cab31088ec440
mohamed azoz <125761608+MohamedAzoz@users.noreply.github.com>
2025-01-21 21:18:13 +0200

    all

---
+++ src/app/directive/viwe.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ViweDirective } from './viwe.directive';
+
+describe('ViweDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ViweDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directive/viwe.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appViwe]'
+})
+export class ViweDirective {
+
+  constructor() { }
+
+}
